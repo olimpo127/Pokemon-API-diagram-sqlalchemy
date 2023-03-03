c032f3a087386860024062bc3514897eab58b839
F.Ojeda.I <fojedai47@gmail.com>
2023-03-03 23:29:35 +0000

    user added

--- src/models.py
+++ src/models.py
@@ -8,12 +8,13 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-#class Person(Base):
-    #__tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    #id = Column(Integer, primary_key=True)
-    #name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    favorite = relationship("Favorite")
 
 #class Address(Base):
     #__tablename__ = 'address'
@@ -69,6 +70,7 @@ class Favorite(Base):
     pokemon_id = Column(Integer)
     name = Column(String(50), nullable=False) 
     parent_id = Column(Integer, ForeignKey("pokemon.pokemon_id"))
+    parent_id = Column(Integer, ForeignKey("user.id"))
 
 
 
